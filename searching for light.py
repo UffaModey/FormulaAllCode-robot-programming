ef53e1037c3b6e06ca39642ece00598830067bb9
Uffa Modey <83600229+UffaModey@users.noreply.github.com>
2021-05-19 08:17:31 +0100

    Add files via upload

---
+++ searching for light.py
@@ -0,0 +1,46 @@
+import FA
+import time
+
+fa = FA.Create()
+
+fa.ComOpen(6)
+
+fa.LCDClear()
+treshold = 3000
+pos = "A"
+movement = 5
+
+while True:
+    illumination = fa.ReadLight()
+    print(illumination)
+
+    if (illumination > treshold):
+        pos = "A"
+        fa.Forwards(100)
+
+    else:
+        if pos == "A":
+            pos = "B"
+            fa.Left (movement)
+        elif pos == "B":
+            pos = "C"
+            fa.Right(movement)
+        elif pos == "C":
+            pos = "D"
+            fa.Right(movement)
+        else:
+            pos = "A"
+            fa.Left (movement)
+
+            if movement > 180:
+                movement = movement + 5
+
+            else:
+                movement = 5
+    time.sleep(2)
+    break
+            
+    
+    
+
+fa.ComClose()
